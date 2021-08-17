402d0c523ac29265e14fffa5055e4f4cf6b7864c
neharajesh <leehanseok3@gmail.com>
2021-08-17 23:04:12 +0530

    FIX: Minor Bug

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import themeReducer from "../features/Theme/themeSlice"
 import authReducer from "../features/Auth/authSlice"
 import userReducer from "../features/Users/userSlice"
@@ -7,7 +6,6 @@ import postReducer from "../features/Posts/postSlice"
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     theme: themeReducer,
     auth: authReducer,
     users: userReducer,
